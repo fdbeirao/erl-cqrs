32279b04c77ff51b7a1e4a004ae196febb15ade0
Fábio Beirão <fdbeirao@gmail.com>
2017-02-06 20:50:40 +0100

    Very simple and naive implementation of a logger

---
+++ src/logger.erl
@@ -0,0 +1,16 @@
+-module(logger).
+-compile([export_all]).
+
+%% Public API
+
+unexpected_message_on_handle_call(Module, Message, FromPid) ->
+  io:format("Unexpected message on [handle_call] for module ~p, coming from Pid ~p: [~p] ~n", [Module, FromPid, Message]),
+  ok.
+
+unexpected_message_on_handle_cast(Module, Message) ->
+  io:format("Unexpected message on [handle_cast] for module ~p: [~p]~n", [Module, Message]),
+  ok.
+
+unexpected_message_on_handle_info(Module, Message) ->
+  io:format("Unexpected message on [handle_info] for module ~p: [~p]~n", [Module, Message]),
+  ok.
